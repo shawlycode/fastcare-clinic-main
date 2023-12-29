c358f9f20b01a8368ade5e075ca0b4c9fe9dd6a1
Shawlycode <kutinaorlando@gmail.com>
2023-12-29 01:18:53 +0000

    committed

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import LandingPage from './pages/landingpage/LandingPage';
+import Sidebar from './components/sidebar/Sidebar';
+import Staff from './pages/sidebarPages/Staff';
+import UserAccount from './pages/sidebarPages/UserAccount';
+import ResetPassword from './pages/sidebarPages/ResetPassword';
+import SubscriptionIndividual from './pages/sidebarPages/SubscriptionIndividual';
+import IndividualsAndGroups from './pages/sidebarPages/IndividualsAndGroups';
+import { MakePayments, PaymentsConfirmation, ViewPayments } from './pages/sidebarPages/Payments';
+
+const App = () => {
+  return (
+    <>
+
+      <Routes>
+        <Route path="/" exact element={<LandingPage />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path='/sidebar' element={<Sidebar />} />
+      </Routes>
+      <Sidebar />
+      <Routes>
+
+        <Route path="/staff" element={<Staff />} />
+        <Route path="/userAccount" element={<UserAccount />} />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/subscription/individual" element={<SubscriptionIndividual />} />
+        <Route path="/subscriptions/individuals/groups" element={<IndividualsAndGroups />} />
+        <Route path="/payments/viewPayments" element={<ViewPayments />} />
+        <Route path="/payments/paymentsConfirmation" element={<PaymentsConfirmation />} />
+        <Route path="/payments/makePayments" element={<MakePayments />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
